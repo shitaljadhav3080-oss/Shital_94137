1ffe403092be0f2c1ad591432342874ebe6d250a
Shital Jadhav <shitaljadhav3080@gmail.com>
2026-01-01 21:48:55 -0800

    FINAL_PROJECT

---
+++ FINAL_PROJECT/Smart_Env_Moisture_Monitor/Core/Inc/MQ2.h
@@ -0,0 +1,18 @@
+/*
+ * MQ2.h
+ *
+ *  Created on: Jan 01, 2026
+ *      Author:Admin
+ */
+
+#ifndef INC_MQ2_H_
+#define INC_MQ2_H_
+
+#include "main.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+
+void Smoke_Monitor_Task(void);
+
+#endif /* INC_MQ2_H_ */
---
+++ FINAL_PROJECT/Smart_Env_Moisture_Monitor/Core/Inc/led.h
@@ -0,0 +1,19 @@
+/*
+ * led.h
+ *
+ *  Created on: Jan 30, 2025
+ *      Author: Admin
+ */
+
+#ifndef INC_LED_H_
+#define INC_LED_H_
+
+#include "main.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+
+
+void Led_Alert(void);
+
+#endif /* INC_LED_H_ */
